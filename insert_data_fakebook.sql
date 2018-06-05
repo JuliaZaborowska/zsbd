887f6f1bc1e1e8d76d6510e92e63bf683018453c
Gnlknmd <dominik.lange@outlook.com>
2018-06-05 06:57:09 +0200

    Create db script and insert for some tables

---
+++ insert_data_fakebook.sql
@@ -0,0 +1,59 @@
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (100, 'Zbyszard256', '9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08', 'Zbyszard','Ryszardzki', null, '1988-10-30', 'mê¿czyzna', 'Warszawa', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (101, 'QWERTY', '4E758D4760A6CFFC347CDB45F0966D20F481BAD806731C4C0E44F21CF9D90BB5', 'Jakub','Batyskaf', null, '2005-01-22', 'mê¿czyzna', 'Kraków', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (102, 'WrozbitaMaciej', '1772D0119CC344F719853B2C032C2921398636766EFD3ED2FFBAD1C798E2BD97', 'Maciej','Wró¿bita', null, '2001-10-30', 'mê¿czyzna', '£ódŸ', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (103, 'Miszcz', 'B15DA2F7FCF6A29FF67C4561E0C4A2AD4344389CE77E2F311AD7AEDBB152CC09', 'Jerzy','Kowalski', null, '1989-11-11', 'mê¿czyzna', 'Katowice', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (104, 'Niszczyciel_Swiatow', '4AB253F6F0B50729BE4CB8CD27D34F286BD863BD7B7377401219CB47652F6992', 'Janusz','Cebulski', null, '1999-02-02', 'mê¿czyzna', 'Kraków', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (105, 'Kasia636', '7A1B668B4E40A477AE8106789B26303D9E6E71C1D48637E00EBCFE70F513195D', 'Katarzyna','Wokulska', null, '1974-10-30', 'kobieta', 'Poznañ', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (106, 'XiaomiLepsze', 'F2AF1500FD77F93E84479AB92E09260484E32AE92EDD6EAFE6949C10DD846F4F', 'Sebastian','Daszek', null, '1899-10-10', 'mê¿czyzna', 'Wa³brzych', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (107, 'Robert1', '127C8B51050F8B627270C16E1A8ADFD16D0A3EB5BF1B77E3357D34762C828439', 'Robert','Kubica', null, '1944-01-01', 'mê¿czyzna', 'Czêstochowa', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (108, 'EEEEEEE', '5078C90618764325C705CEFB5E4677EB43709CF97EB64089DCF005AD335DA3C9', 'Bo¿ydar','Jakistam', null, '1988-10-30', 'mê¿czyzna', 'Zgierz', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (109, 'asdf', 'BFFD424CEFEB41618ABDDCB2994A8C7A36E6ED6B94DF03545295F3317472912C', 'Dominik','Lange', null, '1995-07-21', 'mê¿czyzna', '£ódŸ', null)
+insert into Uzytkownik (id, login, haslo, imie, nazwisko, telefon, data_urodzenia, plec, miasto, miejsce_pracy) values (110, 'Zbyszard', '9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08', 'Zbyszard','Magiczny', null, '1928-06-06', 'mê¿czyzna', 'Zielona Góra', null)
+
+insert into Post values (100, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla gravida, tellus a condimentum interdum,
+ arcu lacus ultrices tellus, aliquet congue diam diam sodales tortor. Cras porttitor risus nunc. Orci varius natoque penatibus 
+ et magnis dis parturient montes, nascetur ridiculus mus. Donec porta ligula leo, vel gravida ex mollis et. Vestibulum ac eros interdum, 
+ semper nisi non, lobortis eros. Ut bibendum nisi ut metus finibus, non porta augue posuere. Etiam vulputate, neque non pellentesque 
+ dapibus, est tortor rhoncus tellus, at pellentesque quam ligula eu arcu. Integer at mollis ex. Curabitur at lacus suscipit, 
+ dictum sem vel, commodo elit.', '2018-07-07', 1)
+ 
+ insert into Post values (100, 'Vestibulum hendrerit, odio nec vestibulum posuere, elit felis tincidunt leo, vitae dignissim dui ligula 
+ eget augue. Aenean aliquam dolor ut nunc vehicula sodales. Pellentesque quis varius diam, in faucibus mi. Pellentesque dapibus nunc turpis, a
+ c sagittis tellus luctus sed. Ut luctus sem tortor, id porttitor ipsum consectetur ut. Sed vel nisi vitae magna laoreet euismod. 
+ Etiam imperdiet sapien ac dignissim consectetur. Integer dictum metus in fringilla cursus. Nam sagittis velit vel velit maximus varius.
+  Curabitur eget bibendum sem, ac accumsan sapien. Nam dui odio, blandit quis vestibulum sit amet, lobortis eu eros. 
+  Phasellus suscipit erat augue, a interdum purus fringilla vel.', '2018-07-07', 2)
+
+ insert into Post values (100, 'Sed aliquet ultrices sapien non pellentesque. 
+ Praesent tempus neque mattis erat eleifend, eu fringilla turpis hendrerit. In hac habitasse platea dictumst. 
+ Nullam sem purus, porta in nisi ultricies, mattis euismod sapien. Pellentesque aliquam mauris quis congue lacinia. 
+ Duis condimentum eleifend interdum. Nulla facilisis accumsan eros vitae feugiat.', '2018-07-08', 3)
+
+  insert into Post values (101, 'Proin ut vulputate purus, eu semper metus. Fusce vitae eros metus. 
+  Nullam ullamcorper ornare elit ac bibendum. Aenean faucibus sem at ex tincidunt aliquam. In dui risus,
+   commodo quis mollis vel, mattis at erat. Vivamus suscipit odio non tellus dictum ullamcorper vitae laoreet sem.
+    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis feugiat cursus velit id rhoncus.', '2018-07-08', 4)
+
+	
+  insert into Post values (105, 'Aliquam vitae rutrum sem. Sed aliquet et tellus in egestas. In bibendum pretium aliquet. 
+  Aenean tempus enim sed feugiat aliquet. Donec fermentum pellentesque dui, at mollis metus vehicula id. Proin vel lectus 
+  lacus. Nulla interdum volutpat massa eget elementum. Mauris et mi erat. Suspendisse egestas, mi ac molestie dictum, dolor 
+  erat porttitor sem, a blandit dui tortor vitae tortor. Ut aliquam vel erat et vehicula. Morbi maximus, enim posuere 
+  congue condimentum, erat dui lacinia urna, ac accumsan erat lacus eget justo.', '2018-07-08', 5)
+
+
+insert into Komentarz values (1, 'Sed aliquet et tellus in egestas. In bibendum pretium aliquet.', 104, '2018-09-09', 1)
+insert into Komentarz values (2, 'Aliquam porta neque nisi, eu auctor leo suscipit ac. Vestibulum tristique nisl lacinia elit ullamcorper volutpat. Ut tincidunt suscipit est, vel luctus nunc aliquet ut. ', 101, '2018-09-09', 2)
+insert into Komentarz values (3, 'Donec blandit euismod malesuada. xDD.', 109, '2018-09-09', 3)
+insert into Komentarz values (4, 'Nam a sollicitudin erat. Nulla eu enim sit amet ex convallis vestibulum vitae a neque.', 102, '2018-09-09', 2)
+insert into Komentarz values (5, 'Duis eros tortor, dignissim eu metus nec, auctor pulvinar eros.', 105, '2018-09-09', 2)
+insert into Komentarz values (6, ' Nulla eu enim sit amet ex convallis vestibulum vitae a neque.', 105, '2018-09-10', 2)
+insert into Komentarz values (7, '.', 106, '2018-09-09', 4)
+insert into Komentarz values (8, 'In hac habitasse platea dictumst.', 103, '2018-09-09', 1)
+insert into Komentarz values (9, 'Sed aliquet et tellus in egestas. In bibendum pretium aliquet.', 103, '2019-01-01', 1)
+insert into Komentarz values (10, 'Sed aliquet et tellus in egestas. In bibendum pretium aliquet.', 101, '2020-11-01', 1)
+
+insert into Grupa values (1, 'Grupa1', 'Vivamus massa sem, condimentum in libero id, venenatis ullamcorper odio. Duis a imperdiet justo. Donec ac neque orci. Morbi mattis odio id neque sollicitudin rhoncus. ')
+insert into Grupa values (2, 'Grupa2', 'Donec quam dolor, sollicitudin non eros semper, mollis pulvinar metus. Nam a sollicitudin erat. Nulla eu enim sit amet ex convallis vestibulum vitae a neque. ')
+insert into Grupa values (3, 'Grupa3', 'Fusce vulputate, lorem nec sollicitudin lacinia, ex purus convallis ipsum, vitae blandit purus orci vel massa. In hac habitasse platea dictumst. ')
+
